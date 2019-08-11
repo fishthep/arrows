de6be0efc21767febdd21b0482dc8ef0ab5e2468
fishthep <53994636+fishthep@users.noreply.github.com>
2019-08-11 20:41:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import discord
 from discord.ext import commands
 import asyncio
-from itertools import cycle
-import time
 import random
-from random import *
 import os
 
-import dict
-from dict import *
-
-
 client = discord.Client()
-status = ['COONS', 'COOOOOOONS']
-
-
-async def change_status():
-    await client.wait_until_ready()
-    msgs = cycle(status)
-    
-    while not client.is_closed:
-        current_status = next(msgs)
-        await client.change_presence(activity=discord.Activity(name=current_status))
-        await asyncio.sleep(2)
+a = 0
+b = 0
                
 @client.event
 async def on_ready():
@@ -31,16 +15,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    a = b
+    b = message
     mess = message.content.lower()
     auth = message.author
     channel = message.channel
-    rnd = randint(1,10)
-    if auth == client.user: return
-    else:
-        if rnd == 1:
-            rnd = randint(0,len(coonsdict) - 1)
-            await message.add_reaction(coonsdict[coons[rnd]])
+    if mess == "arrows?":
+        await a.add_reaction('⬅️')
+        await a.add_reaction('➡️')
         
-            
-client.loop.create_task(change_status())
+ 
 client.run(os.environ['BOT_TOKEN'])
